4e44b5e531437775b2ebe8a0f38777e6c4bcd14d
Roman <pencilking2002@gmail.com>
2015-11-20 17:24:28 -0500

    Started doing vine climbing

---
+++ Suzie_Movement_Final/Assets/AttachBall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachBall : MonoBehaviour {
+	
+	public GameObject ball;
+	public GameObject vineAttachPoint;
+	private bool attached;
+	
+	private void OnGUI ()
+	{
+		if (GUI.Button(new Rect(10,10, 100, 30), "Attach Ball"))
+		{
+			attached = true;
+			ball.transform.position = vineAttachPoint.transform.position + new Vector3(0,-0.3f,0);
+			ball.GetComponent<FixedJoint>().connectedBody = vineAttachPoint.GetComponent<Rigidbody>();
+		}
+		if (GUI.Button(new Rect(10,50, 100, 30), "Add force"))
+		{
+			if (attached)
+			{
+				Vector3 force = new Vector3 (Random.Range(1,10), Random.Range(1,10), Random.Range(1,10));
+				ball.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+			}
+		}
+		
+	}
+}
--- Suzie_Movement_Final/Assets/InControl/Source/VersionInfo.cs
+++ Suzie_Movement_Final/Assets/InControl/Source/VersionInfo.cs
@@ -65,7 +65,7 @@ namespace InControl
 				Major = 1,
 				Minor = 5,
 				Patch = 10,
-				Build = 12961
+				Build = 13054
 			};
 		}
 
